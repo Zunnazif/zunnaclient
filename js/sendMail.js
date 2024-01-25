f77f6f9b1afc1079a6f7384131641fbc0b74a7fb
faizunnazif <faizoen75@gmail.com>
2024-01-25 11:16:37 +0700

    edit message email

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,50 +1,61 @@
-import contains from './contains.js'
-let form = document.getElementById('form');
-let submit = document.getElementById('submit');
-let alertBorder = document.getElementById('alert-border');
-let sending = document.getElementById('sending');
+import contains from "./contains.js";
+let form = document.getElementById("form");
+let submit = document.getElementById("submit");
+let alertBorder = document.getElementById("alert-border");
+let sending = document.getElementById("sending");
 
-form.addEventListener('submit', async(e) => {
-    e.preventDefault()
-    let nameTo = document.getElementById('nameTo').value.split(" ").map(item => {
-        return item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase()
-    }).join(" ");
-    let sendTo = document.getElementById('email').value;
-    let contain = await contains();
-
-    alertBorder.classList.add('hidden')
-
-    let bodyFecth = {
-        nameFrom : "Zunna Digital", 
-        sendFrom: "zunna.digital47@gmail.com", 
-        password: "jtli vpnl pcwc gdga",
-        nameTo: nameTo, 
-        sendTo: sendTo,
-        subject: "E-Book",
-        bodyHTML: `<b>Hallo kak ${nameTo} terima kasih sudah order...<br>Mohon bantuannya untuk konfirmasi "Produk Diterima" dikolom chat shopee ya kakk, thanks</b><br><br>${contain}`
-    }   
-    let request = await fetch('https://zunnaserver.vercel.app/email/SendToEmail', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bodyFecth)
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let nameTo = document
+    .getElementById("nameTo")
+    .value.split(" ")
+    .map((item) => {
+      return item.slice(0, 1).toUpperCase() + item.slice(1).toLowerCase();
     })
-    let response = await request.json()
-    console.log(response)
+    .join(" ");
+  let sendTo = document.getElementById("email").value;
+  let contain = await contains();
 
-    if (response.isSucceeded){
-        alertBorder.classList.remove('hidden')
-        sending.innerHTML = response.response
-        submit.disabled = false;
-        submit.classList.add('text-gray-400', 'hover:bg-slate-600');
-        submit.classList.remove('text-black', 'cursor-progress');
-        submit.innerHTML = 'Kirim'
-        document.getElementById('nameTo').value = "";
-        document.getElementById('email').value = "";
-        document.getElementById('skuID').value = "";
+  if (alertBorder.classList.contains("left-0")) {
+    alertBorder.classList.remove("left-0");
+  }
+  alertBorder.classList.add("-left-full");
 
+  let bodyFecth = {
+    nameFrom: "Zunna Digital",
+    sendFrom: "zunna.digital47@gmail.com",
+    password: "jtli vpnl pcwc gdga",
+    nameTo: nameTo,
+    sendTo: sendTo,
+    subject: "E-Book",
+    bodyHTML: `<b>Hallo kak ${nameTo} terima kasih sudah order...<br>semoga suka dengan buku nya ^_^ dan rezeki kakak sekeluarga lancar selalu,<br><br>Happy reading</b><br><br>${contain}`,
+  };
+  let request = await fetch(
+    "https://zunnaserver.vercel.app/email/SendToEmail",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyFecth),
     }
-})
+  );
+  let response = await request.json();
+  console.log(response);
 
+  if (response.isSucceeded) {
+    alertBorder.classList.remove("-left-full");
+
+    alertBorder.classList.add("left-0");
+
+    sending.innerHTML = response.response;
+    submit.disabled = false;
+    submit.classList.add("text-gray-400", "hover:bg-slate-600");
+    submit.classList.remove("text-black", "cursor-progress");
+    submit.innerHTML = "Kirim";
+    document.getElementById("nameTo").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("skuID").value = "";
+  }
+});
--- js/toggleHandler.js
+++ js/toggleHandler.js
@@ -1,62 +1,58 @@
 function humbergerHandler() {
-    let dropdown = document.getElementById('dropdown');
-    
-    if (!dropdown.classList.contains('hidden')){
-        dropdown.classList.add('hidden');
-    } else {
-        dropdown.classList.remove('hidden');
-    };
-};
+  let dropdown = document.getElementById("dropdown");
 
-function changeHandler() {
-    let getNameBook = document.getElementById('getNameBook');
-    let getSKUBook = document.getElementById('getSKUBook');
-    let thSKU = document.getElementById('thSKU');
-    let thNama = document.getElementById('thNama');
-
-    if (getNameBook.classList.contains('hidden')){
-        getNameBook.classList.remove('hidden');
-        thNama.classList.add('bg-gray-800')
-        thSKU.classList.remove('bg-gray-800')
-        getSKUBook.classList.add('hidden');
-        getSKUBook.value = "";
-    } else {
-        getNameBook.classList.add('hidden');
-        getSKUBook.classList.remove('hidden');
-        thNama.classList.remove('bg-gray-800')
-        thSKU.classList.add('bg-gray-800')
-        getNameBook.value = "";
-    };
-    
-    console.log("getNameBook", getNameBook.value)
-    console.log("getSKUBook", getSKUBook.value)
-};
-
-function alertHandler() {
-    let alertBorder = document.getElementById('alert-border');
-
-    alertBorder.classList.add('hidden');
-
-};
-
-function addSKUonInput(sku){
-    let skuID = document.getElementById('skuID');
-
-    skuID.value += ` ${sku} `;
-
+  if (!dropdown.classList.contains("hidden")) {
+    dropdown.classList.add("hidden");
+  } else {
+    dropdown.classList.remove("hidden");
+  }
 }
 
-function upDown(idAddBook, togelUpDown){
-    let addBook = document.getElementById(idAddBook)
-    let togel = document.getElementById(togelUpDown)
+function changeHandler() {
+  let getNameBook = document.getElementById("getNameBook");
+  let getSKUBook = document.getElementById("getSKUBook");
+  let thSKU = document.getElementById("thSKU");
+  let thNama = document.getElementById("thNama");
 
-    if (!addBook.classList.contains('hidden')){
-        addBook.classList.add('hidden')
-        togel.innerHTML = '<i class="fa-solid fa-chevron-down"></i>'
-    } else {
-        addBook.classList.remove('hidden')
-        togel.innerHTML = '<i class="fa-solid fa-chevron-up "></i>'
-    }
+  if (getNameBook.classList.contains("hidden")) {
+    getNameBook.classList.remove("hidden");
+    thNama.classList.add("bg-gray-800");
+    thSKU.classList.remove("bg-gray-800");
+    getSKUBook.classList.add("hidden");
+    getSKUBook.value = "";
+  } else {
+    getNameBook.classList.add("hidden");
+    getSKUBook.classList.remove("hidden");
+    thNama.classList.remove("bg-gray-800");
+    thSKU.classList.add("bg-gray-800");
+    getNameBook.value = "";
+  }
 
+  console.log("getNameBook", getNameBook.value);
+  console.log("getSKUBook", getSKUBook.value);
+}
 
+function alertHandler() {
+  let alertBorder = document.getElementById("alert-border");
+  alertBorder.classList.remove("left-0");
+  alertBorder.classList.add("-left-full");
+}
+
+function addSKUonInput(sku) {
+  let skuID = document.getElementById("skuID");
+
+  skuID.value += ` ${sku} `;
+}
+
+function upDown(idAddBook, togelUpDown) {
+  let addBook = document.getElementById(idAddBook);
+  let togel = document.getElementById(togelUpDown);
+
+  if (!addBook.classList.contains("hidden")) {
+    addBook.classList.add("hidden");
+    togel.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+  } else {
+    addBook.classList.remove("hidden");
+    togel.innerHTML = '<i class="fa-solid fa-chevron-up "></i>';
+  }
 }
